e1465d1d998c31836a3d242083808e9d66e82551
finero-d <finero91@gmail.com>
2021-03-07 18:58:01 +0900

    confilct test02

--- teamtest/src/teamtest/gittest02.java
+++ teamtest/src/teamtest/gittest02.java
@@ -4,5 +4,6 @@ public class gittest02 {
 	public static void main(String[] args) {
 		System.out.println("new branch test");
 		System.out.println("feeling good");
+		System.out.println("maaaaat");
 	}
 }
